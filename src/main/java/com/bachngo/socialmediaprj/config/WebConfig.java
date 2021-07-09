451a13c1851d378333538b8e7fb1995c03443576
BachNG <nlmbao2015@gmail.com>
2021-07-09 17:06:13 +0700

    allow cors

--- src/main/java/com/bachngo/socialmediaprj/config/WebConfig.java
+++ src/main/java/com/bachngo/socialmediaprj/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("https://social-media-app-17f17.web.app")
 		.allowedMethods("*")
 		.maxAge(3600L)
 		.allowedHeaders("*")
